2d0f2ee3ee813e1c7b682d9a6c824b462cf39733
Rasmus Holm <knase@hades.lysator.liu.se>
2015-12-03 15:25:37 +0100

    test.sh: Fixed flags for test script

--- src/remaining/test.sh
+++ src/remaining/test.sh
@@ -1,11 +1,21 @@
 #!/bin/bash
 
-./diesel -a -b -c -f -p ../testpgm/parstest1.d
-./diesel -a -b -c -f -p ../testpgm/parstest2.d
-./diesel -a -b -c -f -p ../testpgm/parstest3.d
+FLAGS="-parser,-semantic"
 
-#semantics
+make
 
-diesel -b -p -f -y -a ../testpgm/semtest1.d
+if [ "$1" == "-parser" ]
+then
+    ./diesel -a -b -c -f -p ../testpgm/parstest1.d
+    ./diesel -a -b -c -f -p ../testpgm/parstest2.d
+    ./diesel -a -b -c -f -p ../testpgm/parstest3.d
+elif [ "$1" == "-semantic" ]
+then
+    ./diesel -b -p -f -y -a ../testpgm/semtest1.d
+    ./diesel -b -p -f -y -a ../testpgm/semtest2.d
+else
+    echo "Invalid or no flag set, use either of following flags: $FLAGS"
+fi
+
 
 exit 0
